a6f6ec19be9159969ed3fc54116b91349f3075d3
GubamO <guibau2303@gmail.com>
2022-08-15 19:33:55 +0200

    calendrier début

--- DisplayDatesHours/script.js
+++ DisplayDatesHours/script.js
@@ -1,4 +1,5 @@
-let date = new Date()
+let date = new Date();
+
 
 // let weekDay = date.getUTCDay;
 // console.log(weekDay);
@@ -7,6 +8,38 @@ const weekDay = document.getElementById('WeekDay');
 const monthDay = document.getElementById('MonthDay');
 const month = document.getElementById('Month');
 const year = document.getElementById('Year');
+
+const hours = document.getElementById('Hours');
+const minutes = document.getElementById('Minutes');
+const seconds = document.getElementById('Seconds');
+
+const days = [
+    'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'
+];
+const months = [
+    'JAN',
+    'FEB',
+    'MAR',
+    'APR',
+    'MAY',
+    'JUN',
+    'JUL',
+    'AUG',
+    'SEP',
+    'OCT',
+    'NOV',
+    'DEC'
+];
+
+weekDay.innerText = days[date.getDay()];
+monthDay.innerHTML = date.getDate();
+month.innerText = months[date.getMonth()];
+year.innerText = date.getFullYear();
+
+hours.innerText = date.getHours();
+minutes.innerHTML = date.getMinutes();
+seconds.innerHTML = date.getSeconds();
+
 
 
 
