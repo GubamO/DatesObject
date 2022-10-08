36f3dcd288edec41e0c9b219c66b0ac895b41dff
GuillaumeB <guibau2303@gmail.com>
2022-10-08 15:23:33 +0200

    end style project

--- DisplayDatesHours/script.js
+++ DisplayDatesHours/script.js
@@ -1,22 +1,32 @@
-let date = new Date();
+let is24H = true;
+let dark = false;
 
+const calendar = document.querySelector('.calendar');
+const calendarDay = document.querySelector('.calendar_day_txt');
+const calendarTodayDay = document.querySelector('.calendar_month_day_txt');
+const calendarMonth = document.querySelector('.calendar_month_month_txt');
+const calendarYear = document.querySelector('.calendar_year_txt');
 
-// let weekDay = date.getUTCDay;
-// console.log(weekDay);
+const timerHours = document.querySelector('.timer_hours');
+const timerMinutes = document.querySelector('.timer_minutes');
+const timerSeconds = document.querySelector('.timer_seconds');
 
-const weekDay = document.getElementById('WeekDay');
-const monthDay = document.getElementById('MonthDay');
-const month = document.getElementById('Month');
-const year = document.getElementById('Year');
-
-const hours = document.getElementById('Hours');
-const minutes = document.getElementById('Minutes');
-const seconds = document.getElementById('Seconds');
-
+calendar.addEventListener('click', () => {
+    dark = !dark;
+    const elemsDark = document.querySelectorAll(dark ? '.light' : '.dark');
+    for(const e of elemsDark)
+    {
+        e.classList.add(dark ? 'dark' : 'light');
+        e.classList.remove(dark ? 'light' : 'dark');
+    }
+})
+timerHours.addEventListener('click', () => {
+    is24H = !is24H;
+})
 const days = [
     'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'
 ];
-const months = [
+const month = [
     'JAN',
     'FEB',
     'MAR',
@@ -31,18 +41,24 @@ const months = [
     'DEC'
 ];
 
-weekDay.innerText = days[date.getDay()];
-monthDay.innerHTML = date.getDate();
-month.innerText = months[date.getMonth()];
-year.innerText = date.getFullYear();
+const updateCalendar = () => {
+    let date = new Date();
+    calendarDay.innerText = days[date.getDay()];
+    calendarTodayDay.innerText = date.getDate();
+    calendarMonth.innerText = month[date.getMonth()];
+    calendarYear.innerText = date.getFullYear();
 
-hours.innerText = date.getHours();
-minutes.innerHTML = date.getMinutes();
-seconds.innerHTML = date.getSeconds();
+    let h = date.getHours();
+    if(!is24H)
+    {
+        h %= 12;
+    }
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    timerHours.innerHTML = h < 10 ? `0${h}` : h;
+    timerMinutes.innerHTML = m < 10 ? `0${m}` : m;
+    timerSeconds.innerHTML = s < 10 ? `0${s}` : s;
+};
+updateCalendar();
 
-
-
-
-
-
-
+setInterval(updateCalendar, 1000);
